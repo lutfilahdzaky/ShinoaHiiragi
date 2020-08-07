3ea206ac3a7f31a101c079b8d8c89f0ec4536cdd
Rizkyawan Ikkyy <58576231+RizkyAkame@users.noreply.github.com>
2020-08-07 18:14:22 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,7 +26,7 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running ShinoaBot")
+LOGS.info("You are running ShinoaHiiragi")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
