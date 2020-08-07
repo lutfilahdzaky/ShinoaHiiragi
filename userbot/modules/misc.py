21d459617675af3c806b61011a4065caa35fcd63
Rizkyawan Ikkyy <58576231+RizkyAkame@users.noreply.github.com>
2020-08-07 18:46:29 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -101,7 +101,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Click here](https://github.com/MoveAngel/One4uBot) to open One4uBot's GitHub page."
+        "[Klik Disini](https://github.com/RizkyAkame/ShinoaHiiragi) Untuk Membuka Halaman Repo Ini"
     )
 
 
