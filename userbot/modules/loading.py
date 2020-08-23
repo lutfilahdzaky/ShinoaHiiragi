11b18cb641e7d6d176fc1e2232ba909318f18714
MoveAngel <moveangel@minaproject.tech>
2020-08-23 06:03:07 +0000

    pylint: auto fixes

--- userbot/modules/loading.py
+++ userbot/modules/loading.py
@@ -1,14 +1,10 @@
-import asyncio
-import re
-import time
 from time import sleep
-from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
 
 @register(outgoing=True, pattern='^.fl(?: |$)(.*)')
 async def typewriter(typew):
-    message = typew.pattern_match.group(1)
+    typew.pattern_match.group(1)
     await typew.edit("Sedang Memulai. Silahkan Tunggu")
     sleep(1)
     await typew.edit("0%")
