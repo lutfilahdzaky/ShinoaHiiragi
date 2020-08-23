5d184595ca8f7efc4742ff443305f7c86552bd5c
MoveAngel <moveangel@minaproject.tech>
2020-08-23 05:39:12 +0000

    pylint: auto fixes

--- userbot/modules/p.py
+++ userbot/modules/p.py
@@ -1,29 +1,22 @@
-import asyncio
-import re
-import time
 from time import sleep
-from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
+
 
 @register(outgoing=True, pattern='^p(?: |$)(.*)')
 async def typewriter(typew):
-	message = typew.pattern_match.group(1)
-	sleep(1)
-	await typew.edit("Biasakanlah Mengucapkan Salam")
-	sleep(1)
-	await typew.edit("Assalamualaikum")
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("Biasakanlah Mengucapkan Salam")
+    sleep(1)
+    await typew.edit("Assalamualaikum")
 # Create by myself @rizkynfs
 
 
 @register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
-	message = typew.pattern_match.group(1)
-	sleep(1)
-	await typew.edit("Biasakanlah Mengucapkan Salam")
-	sleep(1)
-	await typew.edit("Assalamualaikum")
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("Biasakanlah Mengucapkan Salam")
+    sleep(1)
+    await typew.edit("Assalamualaikum")
 # Create by myself @rizkynfs
-
-
-
-
